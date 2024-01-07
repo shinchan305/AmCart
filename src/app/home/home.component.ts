bb9fa7f35739d894571b0dd76d3193b450615f7f
Suraj Agrawal <surajagrawal1794@gmail.com>
2024-01-07 15:11:35 +0530

    Checking if the CI CD works end to end for front end

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   images = [1, 2, 3, 4, 5, 6, 7].map((n) => `../../assets/images/image${n}.jpg`);
   pauseOnHover: boolean = true;
   pauseOnFocus: boolean = false;
+
+  ngOnInit() {
+    console.log(this.images);
+  }
 
   onSlide(e: any) {
 
