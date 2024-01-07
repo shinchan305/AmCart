a5a883612c42be5917c6df595215e5250d0ce313
Suraj Agrawal <surajagrawal1794@gmail.com>
2024-01-07 16:30:29 +0530

    removed code from home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,14 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   images = [1, 2, 3, 4, 5, 6, 7].map((n) => `../../assets/images/image${n}.jpg`);
   pauseOnHover: boolean = true;
   pauseOnFocus: boolean = false;
-
-  ngOnInit() {
-    console.log(this.images);
-  }
 
   onSlide(e: any) {
 
