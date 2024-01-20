dbb7e997615b3f642b84524864db243122f0c60c
Suraj Agrawal <surajagrawal1794@gmail.com>
2024-01-20 21:36:13 +0530

    Contact us css changes

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -16,6 +16,26 @@ export class ContactUsComponent {
       country: "France",
       address: "109 Avenue Léon, 63 Clermont-Ferrand",
       contact: "+12 345-423-9893"
+    },
+    {
+      country: "Canada",
+      address: "304, Tower C, Building No.3",
+      contact: "+91 9033384223"
+    },
+    {
+      country: "Amsterdam",
+      address: "109 Avenue Léon, 63 Clermont-Ferrand",
+      contact: "+12 345-423-9893"
+    },
+    {
+      country: "United States",
+      address: "304, Tower C, Building No.3",
+      contact: "+91 9033384223"
+    },
+    {
+      country: "Poland",
+      address: "109 Avenue Léon, 63 Clermont-Ferrand",
+      contact: "+12 345-423-9893"
     }
   ]
 }
