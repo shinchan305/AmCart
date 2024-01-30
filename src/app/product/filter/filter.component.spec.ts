2bffec6ad89334ea4d91284e2f5d8ce1419b7c74
Suraj Agrawal <surajagrawal1794@gmail.com>
2024-01-30 11:32:49 +0530

    fixed breaking test case

--- src/app/product/filter/filter.component.spec.ts
+++ src/app/product/filter/filter.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SafeHtmlPipe } from 'src/app/shared/pipes/safe-html.pipe';
 
@@ -9,6 +10,7 @@ describe('FilterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ FilterComponent, SafeHtmlPipe ]
     })
     .compileComponents();
