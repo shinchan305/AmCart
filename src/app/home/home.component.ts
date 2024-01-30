fa299bc85572376895b9c3a87de69b077a9099bd
Suraj Agrawal <surajagrawal1794@gmail.com>
2024-01-30 11:30:23 +0530

    Adding c loudfront url of images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  images = [1, 2, 3, 4, 5, 6, 7].map((n) => `../../assets/images/image${n}.jpg`);
+  // images = [1, 2, 3, 4, 5, 6, 7].map((n) => `../../assets/images/image${n}.jpg`);
+  images = [1, 2, 3, 4, 5, 6, 7].map((n) => `https://d295kz4103lnea.cloudfront.net/home/image${n}.jpg`);
   pauseOnHover: boolean = true;
   pauseOnFocus: boolean = false;
 
